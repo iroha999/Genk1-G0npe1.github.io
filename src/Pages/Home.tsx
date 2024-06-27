1be2aa656f91a4d07eb81e092e1aeb28e2488958
zzxi6ha <gongen4090@icloud.com>
2024-06-27 10:22:28 +0900

    Redux add

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,16 @@
 // Home.tsx
+import { useAppSelector, useAppDispatch } from '../Store/hooks';
+import { increment, decrement } from '../Store/Slices/CounterSlice';
 
 function Home() {
+    const count = useAppSelector((state) => state.counter.value);
+    const dispatch = useAppDispatch();
+
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
+            <h1>Count: {count}</h1>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
+// src/main.tsx
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './Store';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
-    <App />
-
-)
+        <Provider store={store}>
+            <App />
+        </Provider>
+);
